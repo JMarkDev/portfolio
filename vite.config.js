6296dbbbc74bce521956a8eced9d7606e1061e38
Josiel Mark M. Seroy <110972366+JMarkDev@users.noreply.github.com>
2025-02-17 22:20:54 +0800

    deploy portfolio

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/portfolio",
 });
-=======
-  base: "/Portfolio"
-})
->>>>>>> 613d5b99d2bb28d1740392b05fb5e97d2cf93fd2
